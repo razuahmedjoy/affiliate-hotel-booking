6bb2a56e49c6b61fa3462c089906a543fa751209
razuahmedjoy <rjjoybzs420@gmail.com>
2025-03-03 23:20:33 +0600

    added www. also

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'https://yolast.vercel.app',
+    'https://www.yolast.com',
     'https://yolast.com',
 ]
 
